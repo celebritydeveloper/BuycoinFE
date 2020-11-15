0b17e7fd84299e2fed35fb8729cd3e7bb059c1e7
Celebrity Developer <essiensaviour.a@gmail.com>
2020-11-15 01:39:06 +0100

    Added HTML structure

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 const language = document.getElementById('language');
+const tab = document.getElementById('tab');
+const main = document.getElementById('main');
+const body = document.getElementById('body');
 const languageModal = document.getElementById('languageModal');
 const closeLanguage = document.getElementById('closeLanguage');
 
@@ -13,3 +16,35 @@ closeLanguage.addEventListener('click', function () {
 languageModal.addEventListener('click', function () {
     languageModal.style.visibility = "hidden";
 });
+
+
+window.addEventListener("scroll", function(e) {
+    if(this.scrollY > 350) {
+       tab.classList.add("tabs--container-fixed", "tabs--list-fixed");
+    }else {
+        tab.classList.remove("tabs--container-fixed", "tabs--list-fixed");
+    }
+});
+
+
+const mediaQuery = window.matchMedia('(min-width: 700px)');
+
+function checkScroll() {
+    if(this.scrollY > 10) {
+        tab.classList.add("tabs--container-fixed", "tabs--list-fixed");
+        console.log("Hello World");
+     }else {
+         tab.classList.remove("tabs--container-fixed", "tabs--list-fixed");
+         console.log("No");
+     }
+}
+
+function handleScreenChange(e) {
+    if(e.matches) {
+        console.log("Yes");
+        checkScroll();
+    }
+}
+
+mediaQuery.addListener(handleScreenChange);
+//handleScreenChange(mediaQuery);
